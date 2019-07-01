f67ba9d17885178a7b459148a4880180ab12eaed
GIT-zhangqi <52399715+GIT-zhangqi@users.noreply.github.com>
2019-07-01 22:50:05 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+//引入express模块
+const express=require('express');
+//创建web服务器
+var app=express();
+//设置监听端口
+app.listne(8080);
---
+++ user.js
@@ -0,0 +1,11 @@
+const express=requirea('express');
+//引入连接池模块
+const pool=require('../pool.js');
+//创建路由器对象
+var router=express.Router();
+//添加路由
+//添加注册路由
+router.post('/reg',function(req,res){
+				
+
+});
